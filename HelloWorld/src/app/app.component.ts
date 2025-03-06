0bbf71d81ad70577dd28d01837406b00e00ada8a
Vardhman Jain <vardhmanjain2003@gmail.com>
2025-03-06 21:26:05 +0530

    [Vardhman Jain] Displaying Error Message

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,40 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,  // <-- Ensure this is added
-  imports: [RouterOutlet, FormsModule],  // <-- Add FormsModule here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
   title = 'Hello From Bridgelabz';
   logoUrl = './assets/images/BridgeLabz.jpg';
   
-  userName: string = "";
-  nameError: string = "";
+  userName: string = '';
+  nameError: string = '';
 
   ngOnInit(): void {
-    this.title = "Hello from brigdeLabz ! ";
+    this.title = "Hello from Bridgelabz!";
   }
 
-  onClick($event:Event){
-    console.log("Saved button on clicked",$event)
+  onClick($event: Event) {
+    console.log("Saved button clicked", $event);
     window.open(this.logoUrl, "_blank");
   }
 
-  // onInput($event:any) {
-  //   console.log("Change event occured!", $event.data);
-  //   const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-  //   if (nameRegex.test(this.userName)) {
-  //     this.nameError = "";
-  //     return;
-  //   }
-  //   this.nameError = "Name is incorrect";
-  // }
+  onInput($event: any) {
+    console.log("Change event occurred!", $event.data);
+    const nameRegex = new RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    this.nameError = nameRegex.test(this.userName) ? '' : 'Name is incorrect';
+  }
 }
-
-
--- HelloWorld/src/app/app.module.ts
+++ HelloWorld/src/app/app.module.ts
@@ -1,19 +1,17 @@
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';  // ✅ Required for ngModel
+import { RouterModule } from '@angular/router'; // ✅ Required for router-outlet
 import { AppComponent } from './app.component';
 
-@NgModule ({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        FormsModule,
-        BrowserModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+@NgModule({
+  declarations: [AppComponent],  // ✅ Now AppComponent can be declared
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-
 export class AppModule { }
