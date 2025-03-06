f9c78f6a674a04ebfede799449f8c1ad9be5a47b
Vardhman Jain <vardhmanjain2003@gmail.com>
2025-03-06 10:45:16 +0530

    [Vardhman Jain] Adding image through binding property

---
+++ HelloWorld/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  //
+  // imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Hello From Bridgelabz';
+  logoUrl = 'https://in.pinterest.com/pin/software-jobs-in-bangalore--1098596902841233586/';
+}
