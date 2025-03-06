933d53710968912579a88c226e46ee1890dbd6c6
Vardhman Jain <vardhmanjain2003@gmail.com>
2025-03-06 11:31:58 +0530

    [Vardhman Jain] Opening on new tab

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -6,9 +6,14 @@ import { RouterOutlet } from '@angular/router';
   //
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hello From Bridgelabz';
-  logoUrl = 'https://in.pinterest.com/pin/software-jobs-in-bangalore--1098596902841233586/';
+  logoUrl = 'https://i.pinimg.com/736x/71/d9/a9/71d9a9a5614ec8a9a17079ace15f10a0.jpg';
+
+  onClick($event:Event){
+    console.log("Saved button on clicked",$event)
+    window.open(this.logoUrl, "_blank");
+  }
 }
